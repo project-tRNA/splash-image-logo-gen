5b4717fd11e66ff3a33439114fce09c653923ec3
TheFatCat <2431208142@qq.com>
2022-05-27 13:42:37 +0800

    decoder basic support (unfinished)

--- Program.cs
+++ Program.cs
@@ -42,10 +42,69 @@ namespace LogoGen
             Environment.Exit(0);
         }
 
+        const int SECTOR_SIZE_IN_BYTES = 512; // Header size
+        public struct SplashHeader
+        {
+            public int width;
+            public int height;
+            public bool compressed;
+            public int real_bytes;
+            public SplashHeader(int width, int height, bool compressed, int real_bytes)
+            {
+                this.width = width;
+                this.height = height;
+                this.compressed = compressed;
+                this.real_bytes = real_bytes;
+            }
+
+            public override string ToString()
+            {
+                return "width: "+width + ", height: " + height + ", type: " + (compressed ? 1 : 0) + ", real_bytes: " + real_bytes;
+            }
+        }
+
+
+        public static SplashHeader? GetImgHeader(byte[] splash)
+        {
+            string magic = "SPLASH!!";
+            int width = 0;
+            int height = 0;
+            bool compressed = splash[16] == 1;
+            int real_size = 0;
+            for (int i = 0; i < SECTOR_SIZE_IN_BYTES; i++)
+            {
+                int b = bunpack(splash[i]);
+                // 检验开头
+                if(i < 8 && splash[i] != magic[i])
+                {
+                    return null;
+                }
+
+                // width
+                if (i >= 8 && i <= 11)
+                {
+                    width += b << ((i - 8) * 8);
+                }
+
+                // height
+                if (i >= 12 && i <= 15)
+                {
+                    height += b << ((i - 12) * 8);
+                }
+
+                // block number
+                if (i >= 20 && i <= 23)
+                {
+                    real_size += b << ((i - 20) * 8);
+                }
+            }
+            int real_bytes = (512 * real_size) - 511;
+            return new SplashHeader(width, height, compressed, real_bytes);
+        }
+
         public static byte[] GetImgHeader(Size size,
             bool compressed = false, int real_bytes = 0)
         {
-            const int SECTOR_SIZE_IN_BYTES = 512; // Header size
             int[] header = new int[SECTOR_SIZE_IN_BYTES];
             Array.Fill(header, 0);
 
@@ -91,7 +150,7 @@ namespace LogoGen
             }
             return output;
         }
-        private static List<Result> encode(List<int> line)
+        public static List<Result> encode(List<int> line)
         {
             int count = 0;
             List<Result> lst = new List<Result>();
@@ -169,22 +228,31 @@ namespace LogoGen
             return lst;
         }
 
-        private static byte[] encodeRLE24(Bitmap img)
+        public static List<int> decode(List<Result> lst)
+        {
+            List<int> line = new List<int>();
+            foreach(Result run in lst) {
+
+            }
+            throw new NotImplementedException();
+            return line;
+        }
+
+        public static byte[] encodeRLE24(Bitmap img)
         {
             int width = img.Width;
             int height = img.Height;
             List<byte> output = new List<byte>();
-
+            Console.WriteLine("width: " + width + ", height: " + height);
             for (int h = 0; h < height; h++)
             {
                 List<int> line = new List<int>();
-                List<Result> result = new List<Result>();
                 for (int w = 0; w < width; w++)
                 {
                     Color c = img.GetPixel(w, h);
                     line.Add((c.R << 16) + (c.G << 8) + c.B);
                 }
-                result = encode(line);
+                List<Result> result = encode(line);
                 foreach (Result r in result)
                 {
                     int count = r.count;
@@ -210,6 +278,76 @@ namespace LogoGen
             return output.ToArray();
         }
 
+        private static Bitmap decodeRLE24(SplashHeader header, byte[] output)
+        {
+            int width = header.width;
+            int height = header.height;
+            Bitmap img = new Bitmap(width, height);
+            int h = -1;
+            int temp_pixel = 0;
+            int step = -1;
+            int count = 0;
+            List<Result> result = new List<Result>();
+            List<int> pixels = new List<int>();
+            for (int i = 0; i < output.Length; i++)
+            {
+                int b = bunpack(output[i]);
+                // count == 256 是每一行的开头
+                if (b == 256)
+                {
+                    if (result.Count > 0)
+                    {
+                        List<int> line = decode(result);
+                        for (int w = 0; w < line.Count; w++)
+                        {
+                            int rgb = line[w];
+                            img.SetPixel(w, h, Color.FromArgb((rgb >> 16) & 0xFF, (rgb >> 8) & 0xFF, rgb & 0xFF));
+                        }
+                        result.Clear();
+                    }
+                    h++;
+                }
+                if (step < 0)
+                {
+                    count = bunpack(output[i]) + 1;
+                    step = 0;
+                    continue;
+                }
+
+                temp_pixel += bunpack(output[i]) << ((step % 3) * 8);
+
+                if (step % 3 == 2)
+                {
+                    if (count > 128)
+                    {
+                       
+                        result.Add(new Result(count, new int[] { temp_pixel }));
+                        temp_pixel = 0;
+                        step = -1;
+                        continue;
+                    }
+                    else
+                    {
+                        // 已确定，当 count <= 128 时，pixel.Length == count
+
+                        // result的数量貌似是和width有关的，为了能够分行，应研究从何处分割行
+                        // 每个 result 的头部必有 count == 256
+                        pixels.Add(temp_pixel);
+                        temp_pixel = 0;
+                        if (step >= count)
+                        {
+                            result.Add(new Result(count, pixels));
+                            pixels.Clear();
+                            step = -1;
+                            continue;
+                        }
+                    }
+                }
+                step++;
+            }
+            return img;
+        }
+
         public static byte[] GetImageBody(Bitmap img, bool compressed = false)
         {
             Bitmap background = new Bitmap(img.Width, img.Height);
@@ -232,11 +370,37 @@ namespace LogoGen
                     }
 
                 }
-                MemoryStream stream = new MemoryStream();
-                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
-                byte[] data = stream.GetBuffer();
-                stream.Close();
-                return data;
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+                    return stream.GetBuffer();
+                }
+            }
+        }
+
+        public static Bitmap GetBodyImage(SplashHeader header, byte[] img)
+        {
+            // REL 压缩
+            if (header.compressed)
+                return decodeRLE24(header, img);
+            else
+            {
+                // 将 bgr 颠倒为 rgb
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    stream.Write(img, 0, img.Length);
+                    Bitmap background = new Bitmap(stream);
+                    Bitmap bitmap = new Bitmap(header.width, header.height);
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        for (int x = 0; x < bitmap.Width; x++)
+                        {
+                            Color c = background.GetPixel(x, y);
+                            bitmap.SetPixel(x, y, Color.FromArgb(c.B, c.G, c.R));
+                        }
+                    }
+                    return bitmap;
+                }
             }
         }
 
@@ -246,10 +410,19 @@ namespace LogoGen
             byte[] header = GetImgHeader(bitmap.Size, SUPPORT_RLE24_COMPRESSIONT, body.Length);
             return Connect(header, body);
         }
-        public static byte[] Connect(params byte[][] bytes) {
+
+        public static Bitmap MakeLogoImage(byte[] singleSplash)
+        {
+            return new Bitmap(1,1);
+        }
+
+        public static byte[] Connect(params byte[][] bytes) 
+        {
             List<byte> output = new List<byte>();
-            foreach (byte[] entry in bytes) {
-                foreach (byte b in entry) {
+            foreach (byte[] entry in bytes)
+            {
+                foreach (byte b in entry) 
+                {
                     output.Add(b);
                 }
             }
@@ -261,8 +434,14 @@ namespace LogoGen
         /// </summary>
         /// <param name="i"></param>
         /// <returns></returns>
-        public static byte bpack(int i) {
+        public static byte bpack(int i)
+        {
             return Convert.ToByte(Convert.ToChar(i));
+        }
+
+        public static int bunpack(byte b)
+        {
+            return Convert.ToInt32(Convert.ToChar(b));
         }
         public struct Result
         {
